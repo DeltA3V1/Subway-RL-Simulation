5ab3c114988a7c69f13663a1a73c56b5515e3670
DeltA3V1 <117963104+DeltA3V1@users.noreply.github.com>
2026-03-30 03:49:57 +0000

    Project 1 finished

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import random
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins. Good for testing, change in production!
+    allow_credentials=False,
+    allow_methods=["*"], # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"], # Allows all headers
+)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/get_step")
+async def read_root():
+    return {"x": random.choice([-10, 0, 10]), "y": random.choice([-10, 0, 10])}
+
+
---
+++ sketch.js
@@ -0,0 +1,44 @@
+let data = { x: 200, y: 200 };
+let path = [];
+let maxPathLength = 100;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    
+    if (frameCount % 10 == 0) {
+        fetchData();
+    
+        path.push({ x: data.x, y: data.y });
+        if (path.length > maxPathLength) {
+            path.shift();
+        }
+    }
+
+    fill(0, 0, 255);
+    rect(data.x - 2.5, data.y - 2.5, 5, 5);
+    noFill();
+    stroke(0);
+    strokeWeight(3);
+    beginShape();
+    for (let i = 0; i < path.length; i++) {
+        vertex(path[i].x, path[i].y);
+    }
+    vertex(data.x, data.y);
+    endShape();
+}
+
+async function fetchData() {
+        try {
+            const response = await fetch('https://reimagined-space-disco-9v7xp7p6x45fpgpr-8000.app.github.dev/get_step');
+            const incoming = await response.json();
+            console.log('Data from backend:', incoming);
+            data.x += (data.x + incoming.x >= 0 && data.x + incoming.x < 400) ? incoming.x : 0;
+            data.y += (data.y + incoming.y >= 0 && data.y + incoming.y < 400) ? incoming.y : 0;
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
